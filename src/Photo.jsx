0f66edb585d6b28edf138dea6f8ce5d4e8567184
Rabia-Rajput <rajput.rabia@outlook.com>
2024-06-11 16:39:43 +0200

    updated file

---
+++ src/Photo.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const Photo = () => {
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+      const data = await res.json();
+      console.log(data);
+      setPhotos(data);
+    };
+
+    fetchPhotos();
+  }, []);
+
+  return <div>
+    <ul> {photos.map(photo=>(
+   <li key={Photo.id}>
+    <h3>{photo.title} </h3>
+    <img src={photo.url} alt={photo.thumbnailUrl} />
+   </li>
+    ))}
+ 
+    </ul>
+  </div>;
+};
+
+export default Photo;
