52ac1b1473575743040be8888845fe19266f8384
Rabia-Rajput <rajput.rabia@outlook.com>
2024-06-16 14:20:40 +0200

    updated file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import Photo from "./Photo";
 import Post from "./Post";
-
+import React from "react";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Post />
-    </div>
+    <Router>
+      <div>
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link to="/photos">Photos</Link>
+          </li>
+        </ul>
+      </nav>
+      </div>
+      <Routes>
+        <Route path="/posts" element={<Post />} />
+        <Route path="/photos" element={<Photo />} />
+        <Route path="/" element={<div className="welcome"><h1>Welcome to Api</h1></div>} />
+      </Routes>
+    
+    </Router>
   );
 }
 
